2a386f7d1c52c95fcfe9f79473c4c37c9ef1613d
kenner kliemann <kenner.hp@gmail.com>
2020-08-26 23:03:32 -0300

    add some seeds for future use

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+plans = [
+  {
+    plan: :fixed,
+    price: 10_000.00
+  },
+  {
+    plan: :flexible,
+    price: 20_000.00
+  },
+  {
+    plan: :prestige,
+    price: 30_000.00
+  }
+]
+
+plans = plans.map do |plan|
+  Subscription.create(plan)
+end
+
+accounts = [
+  {
+    code: 'c8b9194df4',
+    subscription: plans.first
+  },
+  {
+    code: '7deae09e38',
+    subscription: plans.second
+  },
+  {
+    code: 'd81e5f815f',
+    subscription: plans.last
+  }
+]
+
+accounts.each do |acc|
+  Account.create(acc)
+end
